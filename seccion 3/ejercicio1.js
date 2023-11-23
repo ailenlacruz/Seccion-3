98267095b908df527050b4830f8b05137f415445
Ruth La Cruz <125501523+ailenlacruz@users.noreply.github.com>
2023-11-22 20:45:23 -0400

    Add files via upload

---
+++ seccion 3/ejercicio1.js
@@ -0,0 +1,53 @@
+const logomenu = document.getElementById('menuLateral');
+const menu = document.getElementById('aside');
+const exit = document.getElementById('exit');
+
+logomenu.addEventListener('click', function(){
+    menu.classList.add('mostrar')
+})
+
+exit.addEventListener('click', function(){
+    menu.classList.remove('mostrar')
+})
+
+const izquierda = document.getElementsByClassName('izquierda');
+const derecha = document.getElementsByClassName('derecha');
+const container = document.getElementsByClassName('container');
+const imagenes = document.getElementsByClassName('carroimg');
+const carrusel = document.getElementsByClassName('carrusel');
+
+let D = 0;
+let I = 400;
+
+const inicio = document.getElementById('inicio');
+const btnswitch = document.getElementById('switch');
+
+
+btnswitch.addEventListener('click', function(){
+    inicio.classList.toggle('dark');
+    if(btnswitch.classList.toggle('active')){
+        btnswitch.classList.add('return');
+    }else{
+        btnswitch.classList.remove('return');
+    }
+});
+
+derecha[0].addEventListener('click',function(){
+    D += 100
+    carrusel[0].style.marginLeft = '-' + D + '%';
+    if(D>300){
+        D=0
+        carrusel[0].style.marginLeft = '-' + D + '%';
+    }
+})
+
+izquierda[0].addEventListener('click',function(){
+    I -= 100
+    carrusel[0].style.marginLeft = '-' + I + '%';
+    if(I == 0){
+        carrusel[0].style.marginLeft = '-' + I + '%';
+        I = 300
+    }
+})
+
+window.open('popup.html', 'width: 40px', 'height: 40px');
